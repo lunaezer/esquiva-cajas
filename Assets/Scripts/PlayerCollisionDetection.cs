6f4dbb775b32f199337069838993f5325b876fbe
lunaezer <lunaezer22@gmail.com>
2026-05-07 10:35:01 -0300

    playercollision

---
+++ Assets/Scripts/PlayerCollisionDetection.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollisionDetection : MonoBehaviour
+{
+
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.CompareTag("Caja")){
+            
+            Destroy(gameObject);
+        }
+    }
+}
